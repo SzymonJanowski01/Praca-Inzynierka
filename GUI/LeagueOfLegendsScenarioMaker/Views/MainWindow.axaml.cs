1302e371db4d598641347c993a33fb92985488c0
SzymonJanowski <SzymonJanowski01@gmail.com>
2023-06-29 13:49:35 +0200

    initial GUI commit + .qitignore

---
+++ GUI/LeagueOfLegendsScenarioMaker/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LeagueOfLegendsScenarioMaker.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
