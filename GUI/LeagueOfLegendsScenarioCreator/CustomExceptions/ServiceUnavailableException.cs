d9bb407ee8e6b89e4b33fe9ac2dad4cb5f355e52
SzymonJanowski <SzymonJanowski01@gmail.com>
2023-10-05 16:48:34 +0200

    feat(GUI): custom exceptions
    service unavaible exception and user conflixt exception

---
+++ GUI/LeagueOfLegendsScenarioCreator/CustomExceptions/ServiceUnavailableException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegendsScenarioCreator.CustomExceptions
+{
+    public class ServiceUnavailableException : Exception
+    {
+        public ServiceUnavailableException() { }
+
+        public ServiceUnavailableException(string message) : base(message) { }
+
+        public ServiceUnavailableException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ GUI/LeagueOfLegendsScenarioCreator/CustomExceptions/UserConflictException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegendsScenarioCreator.CustomExceptions
+{
+    public class UserConflictException : Exception
+    {
+        public UserConflictException() { }
+
+        public UserConflictException(string message) : base(message) { }
+
+        public UserConflictException(string message, Exception innerEx) : base(message, innerEx) { }
+    }
+}
