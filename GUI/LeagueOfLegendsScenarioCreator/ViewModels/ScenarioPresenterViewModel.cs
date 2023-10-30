6a2c7cbf27498fbcccb41195fc3ab402dadc0ae4
SzymonJanowski <SzymonJanowski01@gmail.com>
2023-10-30 16:08:30 +0100

    feat(GUI): button returning to scenarios list

--- GUI/LeagueOfLegendsScenarioCreator/ViewModels/ScenarioPresenterViewModel.cs
+++ GUI/LeagueOfLegendsScenarioCreator/ViewModels/ScenarioPresenterViewModel.cs
@@ -13,13 +13,21 @@ namespace LeagueOfLegendsScenarioCreator.ViewModels
     {
         [Reactive]
         public MainWindowViewModel? MainWindowContent { get; set; }
-
+        public ReactiveCommand<Unit, Unit> BackCommand { get; private set; }
         public ReactiveCommand<Unit, Unit> LogoutCommand { get; private set; }
         public ScenarioPresenterViewModel(MainWindowViewModel? mainViewModel)
         {
             MainWindowContent = mainViewModel;
 
+            BackCommand = ReactiveCommand.Create(Back);
             LogoutCommand = ReactiveCommand.Create(Logout);
+        }
+
+        public void Back()
+        {
+            MainWindowContent!.Scenario = null;
+
+            MainWindowContent!.ToScenarios();
         }
 
         public void Logout()
