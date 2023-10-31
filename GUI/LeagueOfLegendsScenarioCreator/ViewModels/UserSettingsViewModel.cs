817516c74d9026c3771bf82dc28c8dc1159ca94a
SzymonJanowski <SzymonJanowski01@gmail.com>
2023-10-31 15:58:36 +0100

    feat(GUI): user settings preparation

---
+++ GUI/LeagueOfLegendsScenarioCreator/ViewModels/UserSettingsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegendsScenarioCreator.ViewModels
+{
+    public class UserSettingsViewModel : ViewModelBase
+    {
+        [Reactive]
+        public MainWindowViewModel? MainWindowContent { get; set; }
+
+        public ReactiveCommand<Unit, Unit> LogoutCommand { get; private set; }
+
+        public UserSettingsViewModel(MainWindowViewModel? mainWindowContent)
+        {
+            MainWindowContent = mainWindowContent;
+
+            LogoutCommand = ReactiveCommand.Create(Logout);
+        }
+
+        public void Logout()
+        {
+            MainWindowContent!.LogOut();
+        }
+    }
+}
