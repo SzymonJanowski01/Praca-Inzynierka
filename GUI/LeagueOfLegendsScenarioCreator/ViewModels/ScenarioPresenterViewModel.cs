06014d74df90a9ef564d0af6b08d2693c8886289
SzymonJanowski01 <SzymonJanowski01@gmail.com>
2023-12-18 13:03:06 +0100

    feat(GUI): presenter pictures handling

--- GUI/LeagueOfLegendsScenarioCreator/ViewModels/ScenarioPresenterViewModel.cs
+++ GUI/LeagueOfLegendsScenarioCreator/ViewModels/ScenarioPresenterViewModel.cs
@@ -1,10 +1,14 @@
-﻿using LeagueOfLegendsScenarioCreator.Services;
+﻿using LeagueOfLegendsScenarioCreator.Models;
+using LeagueOfLegendsScenarioCreator.Services;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Reactive;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,16 +18,33 @@ namespace LeagueOfLegendsScenarioCreator.ViewModels
     {
         [Reactive]
         public MainWindowViewModel? MainWindowContent { get; set; }
+
+        [Reactive]
+        public ObservableCollection<PhaseProjector> BluePhases { get; set; }
+
+        [Reactive]
+        public ObservableCollection<PhaseProjector> RedPhases { get; set; }
+
         public ReactiveCommand<Unit, Unit> BackCommand { get; private set; }
         public ReactiveCommand<Unit, Unit> UserSettingsCommand { get; private set; }
         public ReactiveCommand<Unit, Unit> LogoutCommand { get; private set; }
+
         public ScenarioPresenterViewModel(MainWindowViewModel? mainViewModel)
         {
             MainWindowContent = mainViewModel;
+            BluePhases = new ObservableCollection<PhaseProjector>(MatchChampionsNames(true));
+            RedPhases = new ObservableCollection<PhaseProjector>(MatchChampionsNames(false));
 
             BackCommand = ReactiveCommand.Create(Back);
             UserSettingsCommand = ReactiveCommand.Create(UserSettings);
             LogoutCommand = ReactiveCommand.Create(Logout);
+        }
+
+        public IEnumerable<PhaseProjector> MatchChampionsNames(bool firstFive)
+        {
+            IEnumerable<Phase> selectedPhases = firstFive ? MainWindowContent!.Scenario!.Phases!.Take(5) : MainWindowContent!.Scenario!.Phases!.Skip(5);
+
+            return selectedPhases.Select(phase => new PhaseProjector(phase!.MainCharacter!, phase!.FirstAlternaticeCharacter!, phase!.SecondAlternaticeCharacter!));
         }
 
         public async void Back()
