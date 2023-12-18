0e9e287c9563b6b26dc0ddf87d98356b2ee198f9
SzymonJanowski01 <SzymonJanowski01@gmail.com>
2023-12-18 13:02:44 +0100

    feat(GUI): class for pictures handling

---
+++ GUI/LeagueOfLegendsScenarioCreator/Models/PhaseProjector.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using LeagueOfLegendsScenarioCreator.Models;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegendsScenarioCreator.Models
+{
+    public class PhaseProjector : ReactiveObject
+    {
+        public PhaseProjector(string MCName, string FAName, string SAName)
+        {
+            MCImage = LoadImage("Splashes", MCName, "jpg");
+            FirstAltCImage = LoadImage("Icons", FAName, "png");
+            SecAltCImage = LoadImage("Icons", SAName, "png");
+        }
+
+        [Reactive]
+        public Bitmap? MCImage { get; set; }
+
+        [Reactive]
+        public Bitmap? FirstAltCImage { get; set; }
+
+        [Reactive]
+        public Bitmap? SecAltCImage { get; set; }
+
+        private static Bitmap LoadImage(string type, string name, string extension)
+        {
+            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+            string uri = $"avares://LeagueOfLegendsScenarioCreator/Assets/Champions-{type}/{name}.{extension}";
+
+            return new(assets?.Open(new Uri(uri)));
+        }
+    }
+}
