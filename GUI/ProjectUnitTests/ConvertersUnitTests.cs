b09f9fb5f99bf5ecdc9d3f76f671fbd9dc4da349
SzymonJanowski01 <SzymonJanowski01@gmail.com>
2023-12-21 23:58:46 +0100

    feat(Tests): EmailConverter unit tests

---
+++ GUI/ProjectUnitTests/ConvertersUnitTests.cs
@@ -0,0 +1,57 @@
+﻿using LeagueOfLegendsScenarioCreator.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectUnitTests
+{
+    [TestClass]
+    public class EmailFormatConverterTests
+    {
+        [TestMethod]
+        public void Convert_ValidEmail_ReturnsTrue()
+        {
+            // Arrange
+            var converter = new EmailFormatConverter();
+            var email = "test@example.com";
+
+            // Act
+            var result = converter.Convert(email, typeof(bool), null, CultureInfo.CurrentCulture);
+
+            // Assert
+            Assert.IsTrue((bool)result);
+        }
+
+        [TestMethod]
+        public void Convert_InvalidEmail_ReturnsFalse()
+        {
+            // Arrange
+            var converter = new EmailFormatConverter();
+            var email = "invalid_email";
+
+            // Act
+            var result = converter.Convert(email, typeof(bool), null, CultureInfo.CurrentCulture);
+
+            // Assert
+            Assert.IsFalse((bool)result);
+        }
+
+        [TestMethod]
+        public void ConvertBack_ReturnsTrue()
+        {
+            // Arrange
+            var converter = new EmailFormatConverter();
+            var value = "";
+
+            // Act
+            var result = converter.ConvertBack(value, typeof(bool), null, CultureInfo.CurrentCulture);
+
+            // Assert
+            Assert.IsTrue((bool)result);
+        }
+    }
+}
