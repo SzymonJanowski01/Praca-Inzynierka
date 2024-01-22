55b147a07cce2e4c5f20853ee075dd632ad63605
SzymonJanowski01 <SzymonJanowski01@gmail.com>
2024-01-22 19:22:25 +0100

    feat(GUI): phases visual editing and changes storing
    Changeing the image on scenario phases from champions list
    Storing changes in a dictionary
    Improved UniformGridHeight calculations

--- GUI/LeagueOfLegendsScenarioCreator/ViewModels/ScenarioEditorViewModel.cs
+++ GUI/LeagueOfLegendsScenarioCreator/ViewModels/ScenarioEditorViewModel.cs
@@ -29,6 +29,12 @@ namespace LeagueOfLegendsScenarioCreator.ViewModels
         public string? Filter { get; set; }
 
         [Reactive]
+        public Dictionary<int, string>? OriginalPhaseChampions { get; set; }
+
+        [Reactive]
+        public Dictionary<int, string>? NewPhaseChampions { get; set; }
+
+        [Reactive]
         public ObservableCollection<ImageLoader> RequestedChampions { get; set; }
 
         [Reactive]
@@ -40,8 +46,14 @@ namespace LeagueOfLegendsScenarioCreator.ViewModels
         [Reactive]
         public Bitmap? SelectedImage { get; set; }
 
+        [Reactive]
+        public string? SelectedChampionName { get; set; }
 
-        public ReactiveCommand<Bitmap, Unit> SelectChampionImageCommand { get; private set; }
+
+        public ReactiveCommand<ImageLoader, Unit> SelectChampionImageCommand { get; private set; }
+        public ReactiveCommand<PhaseProjector, Unit> SelectMCCommand { get; private set; }
+        public ReactiveCommand<PhaseProjector, Unit> SelectFACommand { get; private set; }
+        public ReactiveCommand<PhaseProjector, Unit> SelectSACommand { get; private set; }
         public ReactiveCommand<Unit, Unit> CancelCommand { get; private set; }
         public ReactiveCommand<Unit, Unit> UserSettingsCommand { get; private set; }
         public ReactiveCommand<Unit, Unit> LogoutCommand { get; private set; }
@@ -51,17 +63,41 @@ namespace LeagueOfLegendsScenarioCreator.ViewModels
             MainWindowContent = mainViewModel;
             SelectedImage = null;
             Filter = string.Empty;
-            UniformGridHeight = 72 * 28;
+            OriginalPhaseChampions = GetOriginalPhaseChampions();
+            NewPhaseChampions = new Dictionary<int, string>();
 
             BluePhases = new ObservableCollection<PhaseProjector>(MatchChampionsNames(true));
             RedPhases = new ObservableCollection<PhaseProjector>(MatchChampionsNames(false));
             RequestedChampions = new ObservableCollection<ImageLoader>(GetChampionsImages()!);
 
-            SelectChampionImageCommand = ReactiveCommand.Create<Bitmap>(SelectChampionImage);
+            UniformGridHeight = 75 * ((RequestedChampions.Count + 5) / 6);
+
+            SelectChampionImageCommand = ReactiveCommand.Create<ImageLoader>(SelectChampionImage);
+            SelectMCCommand = ReactiveCommand.Create<PhaseProjector>(SelectMC);
+            SelectFACommand = ReactiveCommand.Create<PhaseProjector>(SelectFA);
+            SelectSACommand = ReactiveCommand.Create<PhaseProjector>(SelectSA);
             CancelCommand = ReactiveCommand.Create(Cancel);
             UserSettingsCommand = ReactiveCommand.Create(UserSettings);
             LogoutCommand = ReactiveCommand.Create(Logout);
 
+        }
+
+        public Dictionary<int, string> GetOriginalPhaseChampions()
+        {
+            var originalPhaseChampions = new Dictionary<int, string>();
+
+            var index = 1;
+            foreach(var phase in MainWindowContent!.Scenario!.Phases!)
+            {
+                originalPhaseChampions.Add(index, phase.MainCharacter!);
+                index++;
+                originalPhaseChampions.Add(index, phase.FirstAlternaticeCharacter!);
+                index++;
+                originalPhaseChampions.Add(index, phase.SecondAlternaticeCharacter!);
+                index++;
+            }
+
+            return originalPhaseChampions;
         }
 
         public IEnumerable<PhaseProjector> MatchChampionsNames(bool firstFive)
@@ -70,7 +106,8 @@ namespace LeagueOfLegendsScenarioCreator.ViewModels
 
             return selectedPhases.Select(phase => new PhaseProjector(new Image("Icons", phase!.MainCharacter!, "png"),
                                                                      new Image("Icons", phase!.FirstAlternaticeCharacter!, "png"),
-                                                                     new Image("Icons", phase!.SecondAlternaticeCharacter!, "png")));
+                                                                     new Image("Icons", phase!.SecondAlternaticeCharacter!, "png"),
+                                                                     phase.PhaseName!));
         }
 
 
@@ -140,15 +177,104 @@ namespace LeagueOfLegendsScenarioCreator.ViewModels
 
         public void SearchChampion()
         {
-            RequestedChampions = new ObservableCollection<ImageLoader>(GetChampionsImages()!);
-            UniformGridHeight = 72 * (RequestedChampions.Count / 6);
+            RequestedChampions = GetChampionsImages()!;
+            UniformGridHeight = 75 * ((RequestedChampions.Count + 5) / 6);
         }
 
-        public void SelectChampionImage(Bitmap image)
+        public void SelectChampionImage(ImageLoader champion)
         {
-            SelectedImage = image;
-            Debug.WriteLine(SelectedImage);
-            Debug.WriteLine("Im here");
+            SelectedImage = champion.ChampionImage;
+            SelectedChampionName = champion.Name;
+        }
+
+        public void SelectMC(PhaseProjector phase)
+        {
+            if (SelectedImage == null || SelectedChampionName == null)
+            {
+                return;
+            }
+            var phaseName = phase.PhaseName;
+            ReplaceChampion(phaseName, "MainCharacter");
+        }
+
+        public void SelectFA(PhaseProjector phase)
+        {
+            if (SelectedImage == null || SelectedChampionName == null)
+            {
+                return;
+            }
+            var phaseName = phase.PhaseName;
+            ReplaceChampion(phaseName, "FirstAlternative");
+        }
+
+        public void SelectSA(PhaseProjector phase)
+        {
+            if (SelectedImage == null || SelectedChampionName == null)
+            {
+                return;
+            }
+            var phaseName = phase.PhaseName;
+            ReplaceChampion(phaseName, "SecondAlternative");
+        }
+
+        public void ReplaceChampion(string phaseName, string champion)
+        {
+            if (phaseName[0] == 'B')
+            {
+                var phase = BluePhases.Where(phase => phase.PhaseName == phaseName).FirstOrDefault();
+                if (champion == "MainCharacter")
+                {
+                    phase!.MCImage = SelectedImage;
+                    var index = 1 + (BluePhases.IndexOf(phase) * 3);
+                    RecordChange(index, SelectedChampionName!);
+                }
+                else if (champion == "FirstAlternative")
+                {
+                    phase!.FirstAltCImage = SelectedImage;
+                    var index = 2 + (BluePhases.IndexOf(phase) * 3);
+                    RecordChange(index, SelectedChampionName!);
+                }
+                else if (champion == "SecondAlternative")
+                {
+                    phase!.SecAltCImage = SelectedImage;
+                    var index = 3 + (BluePhases.IndexOf(phase) * 3);
+                    RecordChange(index, SelectedChampionName!);
+                }
+            }
+            else
+            {
+                var phase = RedPhases.Where(phase => phase.PhaseName == phaseName).FirstOrDefault();
+                if (champion == "MainCharacter")
+                {
+                    phase!.MCImage = SelectedImage;
+                    var index = 16 + (RedPhases.IndexOf(phase) * 3);
+                    RecordChange(index, SelectedChampionName!);
+                }
+                else if (champion == "FirstAlternative")
+                {
+                    phase!.FirstAltCImage = SelectedImage;
+                    var index = 17 + (RedPhases.IndexOf(phase) * 3);
+                    RecordChange(index, SelectedChampionName!);
+                }
+                else if (champion == "SecondAlternative")
+                {
+                    phase!.SecAltCImage = SelectedImage;
+                    var index = 18 + (RedPhases.IndexOf(phase) * 3);
+                    RecordChange(index, SelectedChampionName!);
+                }
+            }   
+        }
+
+        public void RecordChange(int index, string championName)
+        {
+            if (NewPhaseChampions!.ContainsKey(index))
+            {
+                NewPhaseChampions[index] = championName;
+            }
+            else
+            {
+                NewPhaseChampions!.Add(index, championName);
+            }
         }
 
         public async void Cancel()
