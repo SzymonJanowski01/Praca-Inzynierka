83116877e91a2d46f838c148ac253ed5f4085d59
SzymonJanowski01 <SzymonJanowski01@gmail.com>
2024-03-21 12:31:39 +0100

    feat(RecomSys): data constants now includes bans

--- RecommendationSystem/data_constants.py
+++ RecommendationSystem/data_constants.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from icecream import ic
 
 data = pd.read_csv(r'D:\GitHubDesktop - new\Praca-Inzynierka\DataGathering\most_recent_games.csv')
 
@@ -14,10 +15,29 @@ champions_ids = [1, 10, 101, 102, 103, 104, 105, 106, 107, 11, 110, 111, 112, 11
                  76, 77, 777, 78, 79, 8, 80, 81, 82, 83, 84, 85, 86, 875, 876, 887, 888, 89, 895, 897, 9, 90, 901, 902,
                  91, 910, 92, 950, 96, 98, 99]
 
-data_eu = data[data['Region'] == 'LEC'].drop(columns=['Region'])
-data_na = data[data['Region'] == 'LCS'].drop(columns=['Region'])
-data_kr = data[data['Region'] == 'LCK'].drop(columns=['Region'])
-data_cn = data[data['Region'] == 'LPL'].drop(columns=['Region'])
 
-data_eu.to_csv("data_eu.csv", index=False)
+bans_eu = data[data['Region'] == 'LEC'].drop(columns=['Region'])
+bans_eu = bans_eu['Bans']
+bans_na = data[data['Region'] == 'LCS'].drop(columns=['Region'])
+bans_na = bans_na['Bans']
+bans_kr = data[data['Region'] == 'LCK'].drop(columns=['Region'])
+bans_kr = bans_kr['Bans']
+bans_cn = data[data['Region'] == 'LPL'].drop(columns=['Region'])
+bans_cn = bans_cn['Bans']
+
+data_eu = data[data['Region'] == 'LEC'].drop(columns=['Region', 'Bans'])
+data_na = data[data['Region'] == 'LCS'].drop(columns=['Region', 'Bans'])
+data_kr = data[data['Region'] == 'LCK'].drop(columns=['Region', 'Bans'])
+data_cn = data[data['Region'] == 'LPL'].drop(columns=['Region', 'Bans'])
+
+
+if __name__ == "__main__":
+    ic(len(data_cn))
+    ic(len(data_eu))
+    ic(len(data_na))
+    ic(len(data_kr))
+
+    data_cn.to_csv("data_cn.csv", index=False)
+    bans_cn.to_csv("bans_cn.csv", index=False)
+
 
