42f8774732d55c1c7c267616e3f61bf4b6bd8f01
SzymonJanowski01 <SzymonJanowski01@gmail.com>
2024-03-24 23:45:18 +0100

    feat(API): sys path for imports

--- API/app/run.py
+++ API/app/run.py
@@ -1,3 +1,8 @@
 from app import init_app
+from os import path
+import sys
+
+sys.path.append(path.abspath("D:\GitHubDesktop - new\Praca-Inzynierka"))
+sys.path.append(path.abspath("D:\GitHubDesktop - new\Praca-Inzynierka\RecommendationSystem"))
 
 server = init_app()
